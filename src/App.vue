2249ed5b99a03b5ed66f2b0d030eff1c66dda55c
peruibeloko <cmendonca32@gmail.com>
2023-10-12 16:29:23 -0300

    update template

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Content from "./components/Content.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>Project name</header>
+  <main>
+    Project content
+    <Content />
+  </main>
+  <footer>
+    <hr />
+    <a href="http://github.com/peruibeloko/template-vue">Link to source</a>
+  </footer>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+html,
+body,
+#app {
+  height: 100%;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+#app {
+  justify-content: center;
+  align-items: center;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+header,
+main,
+footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+main {
+  flex-grow: 1;
+}
+
+header,
+footer {
+  height: 5rem;
+  width: 100%;
+}
+
+footer hr {
+  width: 80%;
+}
+
+footer a {
+  margin-top: 1rem;
+}
+
+* {
+  background-color: transparent;
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+
+html,
+body {
+  background-color: var(--background);
+  color: var(--foreground);
 }
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <button type="button" @click="count++">count is {{ count }}</button>
+</template>
+
+<style scoped>
+button {
+  border: none;
+  color: var(--foreground);
+  font-family: monospace;
+  font-size: 2rem;
+  border: 5px solid var(--main);
+  padding: 1rem;
+  border-radius: 1rem;
+  transition-duration: 200ms;
+}
+
+button:hover {
+  color: var(--background);
+  background-color: var(--foreground);
+}
+</style>
